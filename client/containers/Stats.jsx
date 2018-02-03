2bd9bfa5a76ff884219d6535ad026d180cd064f8
Teemu Pääkkönen <oamaok@gmail.com>
2018-02-03 10:25:37 +0200

    Add two more stats

--- client/containers/Stats.jsx
+++ client/containers/Stats.jsx
@@ -11,9 +11,16 @@ function Stats({ beers: rawBeers, bars, types }) {
   }))
   .filter(beer => !isNaN(beer.volume) && !isNaN(beer.abv))
 
+  if (beers.length === 0 || bars.lengt === 0 || types.length === 0) {
+    return null;
+  }
+
+
   const totalBeers = beers.length;
   const highestAbv = beers.reduce(R.maxBy(R.prop('abv')), { name: '-', abv: 0 });
   const lowestAbv = beers.reduce(R.minBy(R.prop('abv')), { name: '-', abv: Infinity });
+
+
 
   const totalBeer = beers.reduce((acc, { volume }) => acc + volume , 0)
   const totalAlcohol = beers.reduce((acc, { volume, abv }) => acc + volume * abv * 0.01, 0)
@@ -22,7 +29,14 @@ function Stats({ beers: rawBeers, bars, types }) {
   const favoriteStyle = R.nth(R.map(R.head, R.sortBy(([, n]) => -n.length, styleGroups))[0], types);
 
   const barGroups = R.toPairs(R.groupBy(R.prop('bar'), beers));
-  const favoriteBar = R.nth(R.map(R.head, R.sortBy(([, n]) => -n.length, styleGroups))[0], bars);
+
+  const barVolumes = barGroups.map(([a, b]) => [a, R.sum(R.map(R.prop('volume'), b))]);
+  const [ popularBarId, popularBarVolume ] = R.last(R.sortBy(R.last, barVolumes));
+
+
+  const nameGroups = R.toPairs(R.groupBy(R.prop('name'), beers));
+  const nameVolumes = nameGroups.map(([a, b]) => [a, R.sum(R.map(R.prop('volume'), b))]);
+  const [ drinker, drinkerVolume ] = R.last(R.sortBy(R.last, nameVolumes));
 
   return (
     <div className="stats">
@@ -30,6 +44,10 @@ function Stats({ beers: rawBeers, bars, types }) {
       <div className="stat">
         <div className="label">Total beers consumed:</div>
         <div className="value"><b>{totalBeers}</b></div>
+      </div>
+      <div className="stat">
+        <div className="label">Largest consumer by volume:</div>
+        <div className="value"><b>{drinker}</b> with <b>{drinkerVolume.toFixed(2)} liters</b></div>
       </div>
       <div className="stat">
         <div className="label">Highest ABV beer:</div>
@@ -52,8 +70,8 @@ function Stats({ beers: rawBeers, bars, types }) {
         <div className="value"><b>{favoriteStyle}</b></div>
       </div>
       <div className="stat">
-        <div className="label">Favorite bar by beers:</div>
-        <div className="value"><b>{favoriteBar}</b></div>
+        <div className="label">Favorite bar by volume:</div>
+        <div className="value"><b>{bars[popularBarId]}</b> with <b>{popularBarVolume.toFixed(2)} liters</b></div>
       </div>
     </div>
   );
