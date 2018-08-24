0f8fd2584541ab2cc4a9421c738cb254ca290d5e
Teemu Pääkkönen <oamaok@gmail.com>
2018-08-24 13:54:35 +0300

    Delete unused components

--- client/containers/AddBeerView.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AddBeerView() {
-  return <div className="add-beer-view" />;
-}
-
-export default AddBeerView;
--- client/containers/BarListView.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function BarListView() {
-  return <div className="bar-list-view" />;
-}
-
-export default BarListView;
