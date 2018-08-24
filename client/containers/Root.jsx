6b5206855b26bedfe02668e2df0c1052eb46e479
Teemu Pääkkönen <oamaok@gmail.com>
2018-08-24 14:02:39 +0300

    Reorganize application initialization

--- client/containers/Root.jsx
+++ client/containers/Root.jsx
@@ -5,7 +5,6 @@ import { createRouteNodeSelector } from 'redux-router5';
 import IndexView from 'containers/IndexView';
 import LoginView from 'containers/LoginView';
 import BarView from 'containers/BarView';
-import BarListView from 'containers/BarListView';
 
 function Root({ route, app }) {
   if (!app.hasAuthenticated) {
@@ -17,8 +16,6 @@ function Root({ route, app }) {
       return <IndexView />;
     case 'bar':
       return <BarView params={route.params} />;
-    case 'bars':
-      return <BarListView />;
 
     default:
       // TODO: Implement NotFoundView?
--- client/index.jsx
+++ client/index.jsx
@@ -12,46 +12,55 @@ import Root from 'containers/Root';
 import routes from './routes';
 
 import {
-  fetchBeers, setName, fetchBars, fetchBeerTypes,
+  fetchBeers,
+  setName,
+  fetchBars,
+  fetchBeerTypes,
 } from './actions';
 import reducer from './reducer';
 
+function initializeRouter() {
+  const router = createRouter(routes, {
+    defaultRoute: 'index',
+  });
 
-const router = createRouter(routes, {
-  defaultRoute: 'index',
-});
+  router.usePlugin(browserPlugin({
+    useHash: true,
+  }));
 
-router.usePlugin(browserPlugin({
-  useHash: true,
-}));
-
-const store = createStore(
-  combineReducers({
-    router: router5Reducer,
-    app: reducer,
-  }),
-  applyMiddleware(
-    router5Middleware(router),
-    ReduxThunk,
-  ),
-);
-
-store.dispatch(fetchBars());
-store.dispatch(fetchBeerTypes());
-
-(function beerRefreshLoop() {
-  store.dispatch(fetchBeers());
-  setTimeout(beerRefreshLoop, 1000 * 60);
-}());
-
-const name = localStorage.getItem('ebh-name');
-if (name) {
-  store.dispatch(setName(name));
+  return router;
 }
 
-router.start();
+function initializeStore(router) {
+  const store = createStore(
+    combineReducers({
+      router: router5Reducer,
+      app: reducer,
+    }),
+    applyMiddleware(
+      router5Middleware(router),
+      ReduxThunk,
+    ),
+  );
 
-const render = () => {
+  store.dispatch(fetchBars());
+  store.dispatch(fetchBeerTypes());
+
+  (function beerRefreshLoop() {
+    store.dispatch(fetchBeers());
+    setTimeout(beerRefreshLoop, 1000 * 60);
+  }());
+
+  // Check if we have a name present in the localStorage and initialize the name with it
+  const name = localStorage.getItem('ebh-name');
+  if (name) {
+    store.dispatch(setName(name));
+  }
+
+  return store;
+}
+
+function render(store) {
   ReactDOM.render(
     <AppContainer>
       <Provider store={store}>
@@ -60,12 +69,21 @@ const render = () => {
     </AppContainer>,
     document.querySelector('#root'),
   );
-};
-
-render();
-
-if (module.hot) {
-  module.hot.accept('containers/Root', () => {
-    render();
-  });
 }
+
+function initializeApplication() {
+  const router = initializeRouter();
+  const store = initializeStore(router);
+
+  router.start();
+  render(store);
+
+  // Initialize hot module reloading
+  if (module.hot) {
+    module.hot.accept('containers/Root', () => {
+      render();
+    });
+  }
+}
+
+initializeApplication();
