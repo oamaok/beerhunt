e6eced82e999cb54abad8a0ab9f0b26896f42b2b
Eelis Autio <eelis.autio@gmail.com>
2018-10-25 00:20:56 +0300

    Add: more statistics

--- client/components/live-stats-view/live-stats-view.jsx
+++ client/components/live-stats-view/live-stats-view.jsx
@@ -41,29 +41,55 @@ function LiveStatsView({ beers, bars, beerTypes }) {
 
   const totalBeers = beers.length;
   const totalBeerVolume = beers.reduce((acc, { volume }) => acc + volume, 0);
-  const grouped = R.toPairs(R.groupBy(R.prop('personName'), beers));
+  const totalBeerPrice = beers.reduce((acc, { price }) => acc + price, 0);
+  const groupedPerson = R.toPairs(R.groupBy(R.prop('personName'), beers));
+  const groupedType = R.toPairs(R.groupBy(R.prop('typeId'), beers));
 
-  const personWithMostBeers = grouped.reduce(R.maxBy(([, beers]) => beers.length),
-    ['-', []]);
+  const personWithMostBeers = groupedPerson.reduce(R.maxBy(([, beers]) => beers.length), ['-', []]);
+  const typeWithMostBeers = groupedType.reduce(R.maxBy(([, beers]) => beers.length), ['-', []]);
   const personWithPriciestBeer = beers.reduce((currentMax, beer) => {
     if (currentMax.price > beer.price) {
       return currentMax;
     }
-    return { name: beer.personName, price: beer.price };
-  }, { name: '-', price: 0 });
+    return {
+      name: beer.personName,
+      price: beer.price,
+      type: beerTypes[beer.typeId],
+    };
+  }, { name: '-', price: 0, type: '-' });
+
+  // console.log(groupedPerson);
+  console.log(typeWithMostBeers);
+  // console.log(personWithPriciestBeer);
+
+  const fullPriceLabel = totalBeerPrice > 1000 ? 'Kokonaishinta (v***n juopot)' : 'Kokonaishinta';
 
   return (
     <div>
       <h1>Live-tulokset</h1>
       <div className={css('status-blocks')}>
-        <StatusBlock width="1" height="1" label="Oluita juotu yhteensä (kpl)" value={totalBeers} />
+        {typeWithMostBeers[0] != '-'
+          ? <StatusBlock width="1" height="1" label="Suosituin juomatyyppi" value={`${beerTypes[typeWithMostBeers[0]]}`} /> : null
+        }
+        <StatusBlock width="1" height="1" label="Juotuja juomia yhteensä (kpl)" value={totalBeers} />
         <StatusBlock width="1" height="1" label="Kokonaismäärä" value={`${totalBeerVolume}l`} />
-        {personWithMostBeers
-          ? <StatusBlock width="2" height="1" label="Eniten juotuja oluita" value={`${personWithMostBeers[0]} - ${personWithMostBeers[1].length}`} /> : null
-      }
-        {personWithPriciestBeer
-          ? <StatusBlock width="2" height="1" label="Kallein olut henkilöllä" value={`${personWithPriciestBeer.name} - ${personWithPriciestBeer.price}€`} /> : null
-      }
+        <StatusBlock width="1" height="1" label={fullPriceLabel} value={`${totalBeerPrice}€`} />
+        {personWithMostBeers[0] != '-'
+          ? <StatusBlock width="2" height="1" label="Eniten juotuja juomia" value={`${personWithMostBeers[0]} - ${personWithMostBeers[1].length}`} /> : null
+        }
+        {personWithPriciestBeer.name != '-'
+          ? (
+            <StatusBlock
+              width="2"
+              height="1"
+              label="Kallein juoma"
+              value={`
+            ${personWithPriciestBeer.name} - 
+            ${personWithPriciestBeer.type} / 
+            ${personWithPriciestBeer.price}€`}
+            />
+          ) : null
+        }
       </div>
 
       <h3>Viisi viimeisintä</h3>
